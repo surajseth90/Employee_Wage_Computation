aac92f31bf5d1dbccd8cbcef70431ece6c82b0d0
surajseth90 <surajseth90@gmail.com>
2021-04-19 16:43:40 +0530

    code to calculate employee wage by switch case is added successfully

--- PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
+++ PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
@@ -1,31 +1,41 @@
 public class Employee_Wage_Computation {
 	
+
 	public static void main(String[] args) {
 	
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int employee_Full_Time = 1;
-		int employee_Part_Time = 2;
+		final int employee_Full_Time = 1;
+		final int employee_Part_Time = 2;
 		int employee_wage=0;
 		int wage_Per_Hour = 20;
 		int hour_per_day =8;
 		int hour_half_day=4;
 	
 			int attendance_Check = (int)(Math.random()*10)%3;
-		
-			if(attendance_Check==employee_Full_Time)
+			
+			switch (attendance_Check)
+			{
+			case employee_Full_Time:
 			{
 				employee_wage = wage_Per_Hour*hour_per_day;
 				System.out.println("Employee is present and the wage of employee is : "+employee_wage);
-			}	
-			else if(attendance_Check==employee_Part_Time)
+				break;
+			
+			}
+			case employee_Part_Time:
 			{
 				employee_wage = wage_Per_Hour*hour_half_day;
 				System.out.println("Employee is present in half of the day"
 						+ " and the wage of employee is : "+employee_wage);
+				break;
 			}
-			else
+			default :
+			{
 				System.out.println("Employee is absent and the wage is : "+employee_wage);
+				break;
+			}
+			}
 		
 	}
 }
