4caef46c64cb33ada49a94a18c578bd5dcac4fdc
surajseth90 <surajseth90@gmail.com>
2021-04-19 19:32:16 +0530

    code changed into Object oriented pattern

--- PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
+++ PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
@@ -1,3 +1,54 @@
+class Employee
+{
+	final int employee_Full_Time = 1;
+	final int employee_Part_Time = 2;
+	int employee_wage=0;
+	int wage_Per_Hour = 20;
+	int hour_per_day =8;
+	int hour_half_day=4;
+	int working_Day =0;
+	int working_Hour=0;
+	int wage ;
+	
+	int Wage_Calculator()
+	{
+
+		while(working_Day<=20 &&
+				working_Hour<=100){
+			
+			int attendance_Check = (int)(Math.random()*10)%3;
+			switch (attendance_Check)
+			{
+			case employee_Full_Time:
+			{
+				wage = wage_Per_Hour*hour_per_day;
+				employee_wage = employee_wage + wage;
+				working_Hour=working_Hour+8;
+				working_Day++;
+				break;
+			
+			}
+			case employee_Part_Time:
+			{
+				wage = wage_Per_Hour*hour_half_day;
+				employee_wage = employee_wage + wage;
+				working_Hour=working_Hour+4;
+				working_Day++;
+				
+				break;
+			}
+			default :
+			{
+				break;
+			}
+			}			
+		
+		}
+	return employee_wage;
+		
+	}
+}
+
 public class Employee_Wage_Computation {
 	
 
@@ -5,49 +56,11 @@ public class Employee_Wage_Computation {
 	
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		final int employee_Full_Time = 1;
-		final int employee_Part_Time = 2;
-		int employee_wage=0;
-		int wage_Per_Hour = 20;
-		int hour_per_day =8;
-		int hour_half_day=4;
-		int working_Day =0;
-		int working_Hour=0;
-		int wage ;
+		
 	
-			
-			while(working_Day<=20 &&
-					working_Hour<=100){
-				
-				int attendance_Check = (int)(Math.random()*10)%3;
-				switch (attendance_Check)
-				{
-				case employee_Full_Time:
-				{
-					wage = wage_Per_Hour*hour_per_day;
-					employee_wage = employee_wage + wage;
-					working_Hour=working_Hour+8;
-					working_Day++;
-					break;
-				
-				}
-				case employee_Part_Time:
-				{
-					wage = wage_Per_Hour*hour_half_day;
-					employee_wage = employee_wage + wage;
-					working_Hour=working_Hour+4;
-					working_Day++;
-					
-					break;
-				}
-				default :
-				{
-					break;
-				}
-				}			
-			
-			}
-			System.out.println("Total wege of employee in the month is : "+employee_wage);
+		Employee employee = new Employee();
+		int employee_wage = employee.Wage_Calculator();
+		System.out.println("Total wage of employee in the month is : "+employee_wage);
 			
 	}
 }
