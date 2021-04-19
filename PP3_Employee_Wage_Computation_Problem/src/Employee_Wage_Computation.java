2c90b2c14b1ca2b4aa43eae2092e6858acec89ad
surajseth90 <surajseth90@gmail.com>
2021-04-19 16:27:20 +0530

    code of part time employee & wage is added successfully

--- PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
+++ PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
@@ -4,18 +4,28 @@ public class Employee_Wage_Computation {
 	
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int employee_present = 1;
+		int employee_Full_Time = 1;
+		int employee_Part_Time = 2;
 		int employee_wage=0;
 		int wage_Per_Hour = 20;
 		int hour_per_day =8;
-		int attendance_Check = (int)(Math.random()*10)%2;
-			if(attendance_Check==employee_present)
+		int hour_half_day=4;
+	
+			int attendance_Check = (int)(Math.random()*10)%3;
+		
+			if(attendance_Check==employee_Full_Time)
 			{
 				employee_wage = wage_Per_Hour*hour_per_day;
 				System.out.println("Employee is present and the wage of employee is : "+employee_wage);
 			}	
+			else if(attendance_Check==employee_Part_Time)
+			{
+				employee_wage = wage_Per_Hour*hour_half_day;
+				System.out.println("Employee is present in half of the day"
+						+ " and the wage of employee is : "+employee_wage);
+			}
 			else
-				System.out.println("Employee is absent");
+				System.out.println("Employee is absent and the wage is : "+employee_wage);
 		
 	}
 }
