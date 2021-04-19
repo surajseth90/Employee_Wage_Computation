e06abdac7caae89a7f1758969039231158558858
surajseth90 <surajseth90@gmail.com>
2021-04-19 15:50:34 +0530

    code to check attendance is added successfully

--- PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
+++ PP3_Employee_Wage_Computation_Problem/src/Employee_Wage_Computation.java
@@ -3,6 +3,11 @@ public class Employee_Wage_Computation {
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
+			
+		int attendance_Check = (int)(Math.random()*10)%2;
+		if(attendance_Check==1)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
